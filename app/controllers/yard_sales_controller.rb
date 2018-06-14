68a8c2a9c9d9aab9af4fd47210e986710106423f
Joe Peck <fatcatt316@yahoo.com>
2018-06-13 21:30:32 -0400

    Hookin' up controller with test method

--- app/controllers/yard_sales_controller.rb
+++ app/controllers/yard_sales_controller.rb
@@ -8,6 +8,8 @@ class YardSalesController < ApplicationController
   def create
     # TODO: Get Date and City from passed in params
     # Figure out what scrapers to use based on City
+    # USE THIS FOR THE REAL ONE
+    # render json: ScrapeManager.yard_sales(city: yard_sale_params[:city], date: yard_sale_params[:date])
     render json: ScrapeManager.test_yard_sales(city: yard_sale_params[:city], date: yard_sale_params[:date])
   end
 
--- app/services/scrape_manager.rb
+++ app/services/scrape_manager.rb
@@ -2,7 +2,11 @@ module ScrapeManager
   extend self
 
   def yard_sales(city: 'Durham', date: Date.today)
-    YardsaleSearch.yard_sales
+    YardsaleSearch.yard_sales(city: city, date: date)
     # TODO: Add other sites
+  end
+
+  def test_yard_sales(city: 'Durham', date: Date.today)
+    YardsaleSearch.test_yard_sales(city: city, date: date)
   end
 end
