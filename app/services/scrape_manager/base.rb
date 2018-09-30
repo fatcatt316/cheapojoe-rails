f95ac77d64b3b5e0e53cc8873f9a9ccfb763e163
Joe Peck <fatcatt316@yahoo.com>
2018-09-30 10:44:34 -0400

    Show yard sales from multiple sites

--- app/services/scrape_manager/base.rb
+++ app/services/scrape_manager/base.rb
@@ -2,10 +2,10 @@ module ScrapeManager::Base
   extend self
 
   def yard_sales(city: 'Durham', date: Date.today)
-    # ScrapeManager::YardsaleSearch.yard_sales(city: city, date: date)
-    ScrapeManager::YardSalesNet.yard_sales(city: city, date: date)
-    # TODO: Add other sites
-    # Craigslist.yard_sales(city: city, date: date)
+    [
+      ScrapeManager::YardsaleSearch.yard_sales(city: city, date: date),
+      ScrapeManager::YardSalesNet.yard_sales(city: city, date: date)
+    ].flatten
   end
 
   def test_yard_sales(city: 'Durham', date: Date.today)
